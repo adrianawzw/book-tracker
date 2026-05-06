1c38235713daf295960e7741c13d0fc058c5f4b1
Adriana Tapia <tapiaadrianalucia@gmail.com>
2026-05-06 15:47:23 -0500

    feat: crea entidad usuario (Closes #10)

--- src/book-tracker/src/main/java/com/booktracker/book_tracker/entities/User.java
+++ src/book-tracker/src/main/java/com/booktracker/book_tracker/entities/User.java
@@ -7,13 +7,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
+//import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users")
-@Builder
+//@Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
