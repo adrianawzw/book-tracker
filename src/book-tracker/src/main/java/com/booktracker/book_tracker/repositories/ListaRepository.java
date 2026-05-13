35435ded8beaae007bfa6f5eb92a47166672b2f2
Angelrg06 <samirrojasgamonal@gmail.com>
2026-05-13 00:38:35 -0500

    feat: crea repository LibroEnLista (Closes #25)

--- src/book-tracker/src/main/java/com/booktracker/book_tracker/repositories/ListaRepository.java
+++ src/book-tracker/src/main/java/com/booktracker/book_tracker/repositories/ListaRepository.java
@@ -1,9 +1,13 @@
 package com.booktracker.book_tracker.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.booktracker.book_tracker.entities.Lista;
 
+@Repository
 public interface ListaRepository  extends JpaRepository<Lista, Long> {
      List<Lista> findByUsuarioId(Long usuarioId);
 }
